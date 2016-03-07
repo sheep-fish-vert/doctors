67597339445651d3cdb66a3fcc3f230064c48445
trolizmaslom <trolizmaslom@gmail.com>
2016-03-07 16:19:47 +0200

    qweqwe

--- src/js/partials/develop_5.js
+++ src/js/partials/develop_5.js
@@ -1,7 +1,47 @@
 try{
+     function scrollTopButton(){
 
+        function showHideScrollButton(){
+
+            console.log($(window).scrollTop()+', '+$(window).height()/2);
+
+            if($(window).scrollTop() > ($(window).height()/2)){
+
+                $('.button-up').addClass('show');
+
+            }
+            else{
+
+                $('.button-up').removeClass('show');
+
+            }
+
+        }
+
+        $(document).on('click', '.button-up.show', function(){
+
+            $(scroller).animate({scrollTop:0},800);
+
+        });
+
+
+        showHideScrollButton();
+
+        $(window).scroll(function(){
+
+            showHideScrollButton();
+
+        });
+
+        $(window).resize(function(){
+
+            showHideScrollButton();
+
+        });
+
+    }
     $(document).ready(function(){
-
+            scrollTopButton();
     });
 
     $(window).load(function(){
