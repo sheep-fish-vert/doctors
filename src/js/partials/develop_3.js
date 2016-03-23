3b16787f028d4f2dec1db46ff5fdcde29c2de820
Art Garm <artem.garmatyk@gmail.com>
2016-03-23 10:43:21 +0200

    add

--- src/js/partials/develop_3.js
+++ src/js/partials/develop_3.js
@@ -1,10 +1,9 @@
 try {
 
     function addHtmlItems(name, size) {
-       var text =  '<div class="convert-item"><div class="item"><div class="close-button-item"></div><div class="convert">        <div class="doc-type"><img src="images/typer1.jpg" alt=""></div><div class="description-file"><div class="name">' + name + '</div><div class="size">' + size + ' </div><div class="done-button"><i class="material-icons">check</i></div></div></div><div class="checker"><div class="done row-check-some"><i class="material-icons">check_box_outline_blank</i><span>Основная</span></div><div class="not-done row-check-some"><i class="material-icons">check_box</i><span>Основная</span>        </div></div > <div class="progress-line"></div></div></div>';
-       console.log(text);
+       var text =  '<div class="convert-item"><div class="item"><div class="close-button-item"></div><div class="convert"><div class="doc-type"><img src="images/typer1.jpg" alt=""></div><div class="description-file"><div class="name">' + name + '</div><div class="size">' + Math.round(size/(1024*1024)*100)/100 + ' Мб' + ' </div><div class="done-button"><i class="material-icons">check</i></div></div></div><div class="checker"><div class="done row-check-some"><i class="material-icons">check_box_outline_blank</i><span>Основная</span></div><div class="not-done row-check-some"><i class="material-icons">check_box</i><span>Основная</span>        </div></div > <div class="progress-line"></div></div></div>';
        return text;
-    }    
+    }
 
     function myFunctionTrain(goGOgo){
         var x = goGOgo.context;
@@ -17,6 +16,12 @@ try {
                 $(goGOgo).closest('form').find('.error-field').addClass('show-this-row');
                 $(goGOgo).closest('form').find('.error-massage').html('Files not selected');
             } else {
+                if ($('.sliding-shift').hasClass('activate-field')){
+                    $('#change-document .sliding-shift').slick('unslick');
+                    $('.sliding-shift').removeClass('activate-field');
+                }               
+                
+
                 for (var i = 0; i < x.files.length; i++) {
                     var betaName = '';
                     var betaSize = '';
@@ -31,7 +36,22 @@ try {
                     $(goGOgo).closest('form').find('.sliding-shift').prepend(addHtmlItems(betaName, betaSize));                    
                 } 
             }
-        } 
+        }
+
+         
+        setTimeout(function() {
+            $('.sliding-shift').addClass('activate-field');
+            $('#change-document .sliding-shift').slick({
+                dots: false,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 3,
+                adaptiveHeight: true,
+                variableWidth: false,
+                centerMode: false
+            });
+        }, 500);
+
         console.log(txt);
     }   
 
@@ -42,11 +62,13 @@ try {
             myFunctionTrain($(this));
         });
                 
-        $('#change-document .item').click(function() {
-            $('#change-document .item').removeClass('active-item');
+        $('#change-document ').on('click', '.convert-item .item' , function() {
+            console.log('click');
+            $('#change-document .convert-item .item').removeClass('active-item');
             $(this).addClass('active-item');
         });
 
+/*        
         $('#change-document .sliding-shift').slick({
             dots: false,
             infinite: false,
@@ -57,7 +79,7 @@ try {
             centerMode: false,
             focusOnSelect: true
         });
- 
+*/
 
     });
 
