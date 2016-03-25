ff71aeb5f008c8515c970a59b2c6112f17f547ff
Art Garm <artem.garmatyk@gmail.com>
2016-03-25 14:09:58 +0200

    gseargwerg

--- src/js/partials/develop_3.js
+++ src/js/partials/develop_3.js
@@ -1,7 +1,10 @@
 try {
 
-    function addHtmlItems(name, size) {
-       var text =  '<div class="convert-item"><div class="item"><div class="close-button-item"></div><div class="convert"><div class="doc-type"><img src="images/typer1.jpg" alt=""></div><div class="description-file"><div class="name">' + name + '</div><div class="size">' + Math.round(size/(1024*1024)*100)/100 + ' Мб' + ' </div><div class="done-button"><i class="material-icons">check</i></div></div></div><div class="checker"><div class="done row-check-some"><i class="material-icons">check_box_outline_blank</i><span>Основная</span></div><div class="not-done row-check-some"><i class="material-icons">check_box</i><span>Основная</span>        </div></div > <div class="progress-line"></div></div></div>';
+    function addHtmlItems(name, size, type, id,renderFile) {
+        if (type == 'image/jpeg') {
+            console.log('its jepeg');
+        }
+       var text =  '<div class="convert-item'+ ' classifire-id-'+ id +'"><div class="item"><div class="close-button-item"></div><div class="convert"><div class="doc-type"><img src="" alt=""></div><div class="description-file"><div class="name">' + name + '</div><div class="size">' + Math.round(size/(1024*1024)*100)/100 + ' Мб' + ' </div><div class="done-button"><i class="material-icons">check</i></div></div></div><div class="checker"><div class="done row-check-some"><i class="material-icons">check_box_outline_blank</i><span>Основная</span></div><div class="not-done row-check-some"><i class="material-icons">check_box</i><span>Основная</span>        </div></div > <div class="progress-line"></div></div></div>';
        return text;
     }
 
@@ -55,36 +58,89 @@ try {
         console.log(txt);
     }   
 
-    var fileStuck = ''; 
+    var fileStuck = '';
+function superUploader() {
+    $("#file-load-area").dmUploader({
+        onInit: function(id, file) {
+            console.log('Plugin successfully initialized');
+        },
+        onBeforeUpload: function(id) {
+            console.log('Starting to upload #' + id);
+        },
+        onUploadProgress: function(id, percent) {
+            console.log('Upload of #' + id + ' is at %' + percent);
+            // do something cool here!
+        },
+        onComplete: function() {
+            console.log('We reach the end of the upload Queue!');
+        },
+        onNewFile: function(id, file) {
+            
+            console.log(file.type);
+            console.log(file.size);
+            console.log(file);
+            console.log(id);
 
-    $(document).ready(function() {
-        $('#change-document input[type=file]').on('change', function() {
-            myFunctionTrain($(this));
-        });
-                
-        $('#change-document ').on('click', '.convert-item .item' , function() {
-            console.log('click');
-            $('#change-document .convert-item .item').removeClass('active-item');
-            $(this).addClass('active-item');
-        });
+            if ($('.sliding-shift').hasClass('activate-field')) {
+                $('#change-document .sliding-shift').slick('unslick');
+                $('.sliding-shift').removeClass('activate-field');
+            };
 
-/*        
-        $('#change-document .sliding-shift').slick({
-            dots: false,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 3,
-            adaptiveHeight: true,
-            variableWidth: false,
-            centerMode: false,
-            focusOnSelect: true
-        });
-*/
-        $("#file-load-area").dmUploader({
-            onInit: function(){
-                console.log('Plugin successfully initialized');
+            //renderFile = file.webkitRelativePath + '/'+file.name;
+            $('#file-load-many').closest('form').find('.sliding-shift').prepend(addHtmlItems(file.name, file.size, file.type, id));
+
+            
+
+            if (typeof FileReader !== "undefined"){
+            
+                var reader = new FileReader();
+
+                // Our <img> object
+                console.log(id);
+
+                var img = $('body').find('#change-document').find('.convert-item').eq(0).find('.doc-type').find('img');
+                console.log(img);
+
+                var renderFile;
+                reader.onload = function (e) {
+                    img.attr('src', e.target.result);
+                }
+                reader.readAsDataURL(file);
+
             }
-        });
+
+
+            
+        }
+    });
+}
+
+$(document).ready(function() {
+    var file;
+    superUploader();
+    $('#change-document input[type=file]').on('change', function() {
+        console.log(file);
+        setTimeout(function() {
+            $('.sliding-shift').addClass('activate-field');
+            $('#change-document .sliding-shift').slick({
+                dots: false,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 5,
+                adaptiveHeight: true,
+                variableWidth: false
+            });
+        }, 500);
+
+
+    });
+
+    $('#change-document ').on('click', '.convert-item .item', function() {
+        console.log('click');
+        $('#change-document .convert-item .item').removeClass('active-item');
+        $(this).addClass('active-item');
+    });
+        
     });
 
     $(window).load(function(){
